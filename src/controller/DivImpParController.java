13c3bd502ddef84d820e93650d1027f3dee79ed3
João Pedro <jplhfernandez@gmail.com>
2025-06-05 22:47:06 -0300

    Ex 10 ED

--- src/controller/DivImpParController.java
+++ src/controller/DivImpParController.java
@@ -2,8 +2,57 @@ package controller;
 
 public class DivImpParController {
 
-	public DivImpParController() {
-		// TODO Auto-generated constructor stub
-	}
+    public DivImpParController() {
 
+    }
+
+    public int[] pegarPares(int[] vetor1, int[] vetor2) {
+        int[] aux = new int[vetor1.length + vetor2.length];
+        int cont = 0;
+
+        for (int num : vetor1) {
+            if (num % 2 == 0) { // par
+                aux[cont++] = num;
+            }
+        }
+
+        for (int num : vetor2) {
+            if (num % 2 == 0) { // par
+                aux[cont++] = num;
+            }
+        }
+
+        // cria vetor resultado do tamanho certo
+        int[] resultado = new int[cont];
+        for (int i = 0; i < cont; i++) {
+            resultado[i] = aux[i];
+        }
+
+        return resultado;
+    }
+
+    // Pega só os números ímpares dos dois vetores juntos
+    public int[] pegarImpares(int[] vetor1, int[] vetor2) {
+        int[] aux = new int[vetor1.length + vetor2.length];
+        int cont = 0;
+
+        for (int num : vetor1) {
+            if (num % 2 != 0) { // ímpar
+                aux[cont++] = num;
+            }
+        }
+
+        for (int num : vetor2) {
+            if (num % 2 != 0) { // ímpar
+                aux[cont++] = num;
+            }
+        }
+
+        int[] resultado = new int[cont];
+        for (int i = 0; i < cont; i++) {
+            resultado[i] = aux[i];
+        }
+
+        return resultado;
+    }
 }
