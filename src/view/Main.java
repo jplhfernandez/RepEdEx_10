ad2cd52d21df84e052b898bfd13c5d021cd29bc3
João Pedro <jplhfernandez@gmail.com>
2025-06-05 22:51:36 -0300

    arrumado

--- src/view/Main.java
+++ src/view/Main.java
@@ -11,8 +11,10 @@ public class Main {
 		int vetor[] = {15, 8, 23, 4, 23, 15, 42, 42, 5, 0, 23, 15, 42};
 		int vetor2[] = {12, 7, 35, 22, 22, 5, 9, 41, 12, 2};
 		DivImpParController imp_par = new DivImpParController();
-		int resultado[] = imp_par.pegarPares(vetor, vetor2);
-		System.out.println("O segundo maior número do vetor é: " + Arrays.toString(resultado));
+		int par[] = imp_par.pegarPares(vetor, vetor2);
+		int impar[] = imp_par.pegarImpares(vetor, vetor2);
+		System.out.println("O vetor par: " + Arrays.toString(par));
+		System.out.println("O vetor impar: " + Arrays.toString(impar));
 	}
 
 }
